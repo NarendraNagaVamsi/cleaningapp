776815557df3107d775a808690daef596e73b070
NarendraNagaVamsi <narendranagavamsi@gmail.com>
2024-12-17 10:56:15 +0530

    changes

--- src/components/Common/FooterComp.vue
+++ src/components/Common/FooterComp.vue
@@ -3,17 +3,34 @@
         <div class="row">
 
             <div class="col-3">
-                hhh
+               <h3>Thecleanic</h3>
             </div>
             <div class="col-3"> 
-sdf
+                <h3>legal</h3>
+                <ul class="list-unstyled">
+                    <li>Privacy Policy</li>
+                    <li>Term & Condition</li>
+                    <li>Refund Policy</li>
+                </ul>
             </div>
             <div class="col-3">
-sdfds
+             <h3>Operating Hours</h3>
+                <ul class="list-unstyled">
+                    <li>Mon - Fri: 8am - 8pm</li>
+                    <li>Saturday: 9am - 7pm   </li> 
+                    <li>Sunday: 9am - 8pm</li>
+                </ul>
 
             </div>
             <div class="col-3">
-sfsdf
+               <h3>Contact</h3>
+               <ul>
+                <li>The cleanic</li>
+                <li>500 Terry Francine Street</li>
+                <li>San Francisco, CA 94158</li>
+                <li>123-456-7890</li>
+                <li>info@mysite.com</li>
+               </ul>
             </div>
 
 
